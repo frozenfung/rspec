a224d0789d9e5e9e0ec9c5724b3060e187150a24
frozenfung <frozenfung@gmail.com>
2014-11-14 10:26:04 +0800

    add train

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Event, :type => :model do
+
+  it "should be valid" do
+    let(:valid_attributes){ { :name => "Train#123"} }
+    expect(Event.new).to be_valid
+    expect(Event.new(valid_attributes)).to_not be_valid
+  end
+end
